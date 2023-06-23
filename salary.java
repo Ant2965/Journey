51355b2b91d949c51c3d8ea3b64ecbdfbc2d79bd
Antriksh <Antriksh>
2023-06-23 23:54:40 +0530

    salary code

---
+++ salary.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class salary {
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Enter your Salary");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a=sc.nextInt();
+		
+		if(a<500000) {
+			
+			System.out.println("tax = 10");
+			}
+		else if(a>500000 && a<100000)
+{
+	         System.out.println("tax = 20");
+}
+         else{
+	         System.out.println("Tax = 30");
+}
+    }
+
+}
