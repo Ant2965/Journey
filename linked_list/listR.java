fc1af1c8ed6279e43fe8ac1fe4eb9876f3b06928
Antriksh <Antriksh>
2023-06-27 00:40:15 +0530

    Linked Lists

---
+++ linked_list/listR.java
@@ -0,0 +1,44 @@
+package linked_list;
+public class listR {
+	public static void call(node head) {
+		node temp=head;
+		if(head==null) {
+			return;
+		}
+		System.out.print(temp.data+"  ");
+		call(head.next);
+		
+		
+	}
+	public static class node{
+		int data;
+		node next;
+		
+		node(int data) {
+			this.data =data;
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		node a =new node(243);
+		node b =new node(2354);
+		node c =new node(2353);
+		node d =new node(2356);
+		node e =new node(23);
+		node f =new node(238);
+		node g =new node(263);
+a.next =b;
+b.next =c;
+c.next=d;
+d.next=e;
+e.next=f;
+f.next=g;
+
+node temp =a;
+call(temp);
+
+	}
+
+}
