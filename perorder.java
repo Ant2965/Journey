f8221faa2f8d4a680a6238c3aa0bb4f64149e985
Ant 2965 <antrikshbahri@gmail.com>
2023-07-23 23:50:22 +0530

    Here i solve predorder traversal

---
+++ perorder.java
@@ -0,0 +1,55 @@
+//package trees;
+
+public class perorder {
+	public static class Node{
+		int data;
+		Node left;
+		Node right;
+		
+		
+		Node(int data){
+			this.data=data;
+			this.left=null;
+			this.right=null;
+		}
+	}
+	public static int index = -1;
+
+	public static Node buildtree(int nodes[]){
+		index+=1;
+		if(nodes[index]== -1) {
+			return null;
+		}
+		
+		
+		Node a =new Node(nodes[index]);
+		a.left=buildtree(nodes);
+		a.right=buildtree(nodes);
+		
+		
+		return a;  
+		
+		
+	}
+	
+	public static void pre(Node root) {
+		if(root==null) {
+			return;
+		}
+		pre(root.left);
+		pre(root.right);
+
+		System.out.println(root.data);
+		
+	}
+	
+	public static void main(String[] args) {
+		int nodes[]= {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+		Node root =buildtree(nodes);
+		//System.out.print(root.data);
+		
+		pre(root);
+	}
+	
+
+}
