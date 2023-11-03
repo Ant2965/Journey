1a4367648101f3e0e8a400bc4b2d539a7b686ac5
Ant2965 <antrikshbahri@gmail.com>
2023-11-03 22:31:38 +0530

    I am back after BMC exam

--- New folder/client/src/components/Header/Header.jsx
+++ New folder/client/src/components/Header/Header.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { TbSearch } from "react-icons/tb";
+import { CgShoppingCart } from "react-icons/cg";
+import { AiOutlineHeart } from "react-icons/ai";
 import "./Header.scss";
+import Search from "./Search/Search";
+import { Context } from "../../utils/context";
+import Cart from "../Cart/Cart";
+
+import "./Header.scss";
+
 const Header = () => {
-    return <div>Header</div>;
+    return <header className="main-header">
+        <div className="header-content"></div>
+    </header>
 };
 
 export default Header;
